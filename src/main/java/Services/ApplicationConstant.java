8dd9cb5222182b9ed44acb39b555db62061c4512
ebrahim <viroos@live.com>
2022-07-30 13:26:49 +0430

    Initial commit

---
+++ src/main/java/Services/ApplicationConstant.java
@@ -0,0 +1,11 @@
+package Services;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class ApplicationConstant {
+    private static Connection connection = new DBHelper().connect();
+    public static Connection getConnection() {return connection;}
+    public static Scanner input = new Scanner(System.in);
+    public  static Scanner getInput(){return input;}
+}
